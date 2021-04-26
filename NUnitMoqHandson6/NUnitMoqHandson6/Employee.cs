ac70276fd3e5e7298635ccd5c0aedbd28b73feac
Jeevitha Ramani <jeevitharamani03@gmail.com>
2021-04-26 14:34:30 +0530

    first commit

---
+++ NUnitMoqHandson6/NUnitMoqHandson6/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+public class EmployeeManager
+{
+    private readonly List<Employee> employees;
+    public int HeadCount { get; set; }
+    public EmployeeManager()
+    {
+        employees = new List<Employee>();
+        HeadCount = employees.Count;
+    }
+    public void AddEmployee(Employee empObj)
+    {
+        if (empObj.Name == null)
+            throw new ArgumentNullException("Name cannot be null");
+        employees.Add(empObj);
+        HeadCount++;
+    }
+}
---
+++ NUnitMoqHandson7/TestLibrary7/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnitMoqHandson7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary7
+{
+    [TestFixture]
+    public class Tests
+    {
+        Calculator c = new Calculator();
+        [TestCase]
+        public void TestCheck_NullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => c.Divide(5, 0));
+        }
+    }
+}
